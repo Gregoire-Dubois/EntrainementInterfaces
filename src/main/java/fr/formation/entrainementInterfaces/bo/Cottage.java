ec19e8dba7c7e282696e60a83c7705d9462205f2
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-05-03 09:19:00 +0200

    Initial commit

---
+++ src/main/java/fr/formation/entrainementInterfaces/bo/Cottage.java
@@ -0,0 +1,2 @@
+package fr.formation.entrainementInterfaces.bo;public class Cottage {
+}
---
+++ src/main/java/fr/formation/entrainementInterfaces/test/Main.java
@@ -0,0 +1,8 @@
+package fr.formation.entrainementInterfaces;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
